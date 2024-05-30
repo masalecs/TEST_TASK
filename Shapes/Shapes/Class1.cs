1fb1d96864acfad492c672d87ca94dcb914f752d
MasAlecs <alecs2018,ru@mail.ru>
2024-05-30 23:45:57 +0300

    Добавил тестовое задание и SQL запрос

---
+++ Shapes/Shapes/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Shape
+    {
+
+        public double TrianglePlosh(int x, int y, int z)
+        {
+            if ((x+y<z) || (x+z<y) || (y+z < x))
+            {
+                return 0;
+            }
+            double plosh;
+            if (Math.Pow(x,2) == Math.Pow(y,2) + Math.Pow(z,2))
+            {
+                plosh = (y * z) / 2;
+                return plosh;
+            }
+            else if (Math.Pow(y, 2) == Math.Pow(x, 2) + Math.Pow(z, 2))
+            {
+                plosh = (x * z) / 2;
+                return plosh;
+            }
+            else if (Math.Pow(z, 2) == Math.Pow(x, 2) + Math.Pow(y, 2))
+            {
+                plosh = (x * y) / 2;
+                return plosh;
+            }
+            int p = (x + y + z) / 2;
+            plosh = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+            return plosh;
+        }
+        public double CirclePlosh(int rad)
+        {
+            return Math.PI * Math.Pow(rad, 2);
+        }
+    }
+}
---
+++ Shapes/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shapes;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double expected = 6;
+            int x = 3;
+            int y = 4;
+            int z = 5;
+
+            Shape triangle = new Shape();
+            double actual = triangle.TrianglePlosh(x,y,z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2() 
+        {
+            double expected = 30;
+            int x = 5;
+            int y = 12;
+            int z = 13;
+            Shape triangle = new Shape();
+            double actual = triangle.TrianglePlosh(x, y, z);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            double expected = Math.PI * Math.Pow(3, 2);
+            int r = 3;
+            Shape circle = new Shape();
+            double actual = circle.CirclePlosh(r);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
